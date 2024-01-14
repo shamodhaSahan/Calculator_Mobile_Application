b29db6a6ae800701537ba80def6d0a72189236b4
shamodhasahan <sahanrathnamalala8@gmail.com>
2024-01-14 19:02:35 +0530

    initial commit

--- App.tsx
+++ App.tsx
@@ -1,117 +1,151 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
+import {useEffect, useState} from 'react';
+import {View, Text, TouchableOpacity, StyleSheet} from 'react-native';
 
-import React from 'react';
-import type {PropsWithChildren} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+const App = () => {
+  const [oldValue, setOldValue] = useState('0');
+  const [newValue, setNewValue] = useState('0');
+  const [operator, setOperator] = useState('');
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+  useEffect(() => {
+    setNewValue(parseFloat(newValue).toString());
+    setOldValue(parseFloat(oldValue).toString());
+  }, [newValue, oldValue]);
 
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
-
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-}
-
-function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  const handleButtonPress = (value: string, isOperator: boolean) => {
+    if (newValue == 'Infinity' || newValue === 'NaN') {
+      setNewValue('0');
+      setOldValue('0');
+      setOperator('');
+    } else if (isOperator) {
+      setOperator(value);
+      if (newValue !== '0') {
+        setOldValue(
+          oldValue === '0' ? newValue : eval(oldValue + operator + newValue),
+        );
+        setNewValue('0');
+      }
+    } else if (value === 'C') {
+      setOldValue('0');
+      setOperator('');
+      setNewValue('0');
+    } else if (value === 'CE') {
+      setNewValue('0');
+    } else if (value === 'DEL') {
+      setNewValue(
+        newValue.length > 1 ? newValue.substring(0, newValue.length - 1) : '0',
+      );
+    } else if (value === '+/-') {
+      setNewValue((parseFloat(newValue) * -1).toString());
+    } else if (value === '.') {
+      setNewValue(!newValue.includes('.') ? newValue + value : newValue);
+    } else if (value === '=') {
+      if (oldValue === '0' && newValue === '0') {
+        setOldValue(newValue);
+        setNewValue('0');
+      } else if (operator) {
+        setOldValue('0');
+        setNewValue(eval(oldValue + operator + newValue));
+        setOperator('');
+      }
+    } else {
+      setNewValue(
+        newValue === '0' || newValue == 'Infinity' || newValue == 'NaN'
+          ? value
+          : newValue + value,
+      );
+    }
   };
 
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
+  const renderButton = (value: string, isOperator = false) => (
+    <TouchableOpacity
+      style={[styles.button, isOperator ? styles.operatorButton : null]}
+      onPress={() => handleButtonPress(value, isOperator)}>
+      <Text style={[styles.buttonText, isOperator && {color: '#fff'}]}>
+        {value}
+      </Text>
+    </TouchableOpacity>
   );
-}
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.displayContainer}>
+        <Text style={styles.displayText}>
+          {oldValue !== '0' && oldValue + operator}
+        </Text>
+        <Text style={styles.displayText}>{newValue}</Text>
+      </View>
+      <View style={styles.buttonContainer}>
+        <View style={styles.buttonRow}>
+          {renderButton('C')}
+          {renderButton('CE')}
+          {renderButton('DEL')}
+          {renderButton('/', true)}
+        </View>
+        <View style={styles.buttonRow}>
+          {renderButton('7')}
+          {renderButton('8')}
+          {renderButton('9')}
+          {renderButton('*', true)}
+        </View>
+        <View style={styles.buttonRow}>
+          {renderButton('4')}
+          {renderButton('5')}
+          {renderButton('6')}
+          {renderButton('-', true)}
+        </View>
+        <View style={styles.buttonRow}>
+          {renderButton('1')}
+          {renderButton('2')}
+          {renderButton('3')}
+          {renderButton('+', true)}
+        </View>
+        <View style={styles.buttonRow}>
+          {renderButton('0')}
+          {renderButton('.')}
+          {renderButton('+/-')}
+          {renderButton('=')}
+        </View>
+      </View>
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  container: {
+    flex: 1,
   },
-  sectionTitle: {
+  displayContainer: {
+    flex: 1,
+    backgroundColor: '#f0f0f0',
+    justifyContent: 'center',
+    alignItems: 'flex-end',
+    padding: 20,
+  },
+  displayText: {
+    fontSize: 30,
+  },
+  buttonContainer: {
+    flex: 2,
+    flexDirection: 'column',
+  },
+  buttonRow: {
+    flex: 1,
+    flexDirection: 'row',
+  },
+  button: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#e0e0e0',
+    borderWidth: 1,
+    borderColor: '#c0c0c0',
+  },
+  operatorButton: {
+    backgroundColor: '#2C2BAB',
+  },
+  buttonText: {
     fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
+    fontWeight:'bold'
   },
 });
 
